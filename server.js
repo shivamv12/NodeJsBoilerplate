53bbc37493265d463d31438502de0a6f651cd30a
shivamv12 <shivambca75@gmail.com>
2020-06-20 14:22:48 +0530

    Implement setup file properties into Server file

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('colors');
 const express = require('express');
 require('dotenv').config({path: './configuration/.env'});
+const {
+  dev: {server},
+} = require('./configuration/setup');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(server.PORT, () => {
   console.log(
-    `\nServer is up on port: ${PORT.bold} with environment: ${process.env.ENVIRONMENT.bold}.\n`
-      .green + `Base URL: http://localhost:${PORT}/\n`.underline
+    `\nServer is up on port: ${server.PORT.bold} with environment: ${server.ENV.bold}.\n`
+      .green + `Base URL: http://localhost:${server.PORT}/\n`.underline
   );
 });
