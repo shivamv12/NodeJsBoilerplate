1a6926a9cc71e2324e9aa672fcaeff13f5dfb3a2
shivamv12 <shivambca75@gmail.com>
2020-06-21 14:36:18 +0530

    Create Auth Validator to validate SignUp req data

---
+++ middlewares/validators/authValidator.js
@@ -0,0 +1,43 @@
+/** NPM Packages */
+const {check, validationResult} = require('express-validator');
+
+/** Validate User Sign Up Request */
+exports.validateSignUp = [
+  check('name')
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('Please enter Name.')
+    .bail()
+    .isLength({min: 2, max: 50})
+    .withMessage('Name must be minimum 2 & maximum 50 characters long.')
+    .bail(),
+  check('email')
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('Please enter Email Address.')
+    .bail()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage('Please enter valid Email Address.')
+    .bail(),
+  check('password')
+    .trim()
+    .escape()
+    .not()
+    .isEmpty()
+    .withMessage('Please enter Password.')
+    .bail()
+    .isLength({min: 6, max: 20})
+    .withMessage('Password must be minimum 6 & maximum 20 characters long.')
+    .bail(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({errors: errors.array()});
+    next();
+  },
+];
