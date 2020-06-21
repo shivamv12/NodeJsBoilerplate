ff0c562be897cd5e3a4976771a1c645f2896b2f6
shivamv12 <shivambca75@gmail.com>
2020-06-21 14:36:53 +0530

    Create sign-up endpoint for user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,11 @@
 /** NPM Packages */
 const router = require('express').Router();
-/** Custom Packages */
-const User = require('../models/User');
 
-router
-  .route('/')
-  .get((req, res) => res.json({msg: 'Get Route'}))
-  .post(async (req, res) => {
-    req.body.uid = 'U-uak32hsk2';
-    const data = await User.create(req.body);
-    return res.status(200).send({msg: 'Post Route', data: data});
-  });
+/** Custom Packages */
+const {validateSignUp} = require('../middlewares/validators/authValidator');
+const {signUp} = require('../controllers/authController');
+
+/** End Points */
+router.route('/sign-up').post(validateSignUp, signUp);
 
 module.exports = router;
