d875e54dcc956199bfc59f16834640d0b064b7b5
shivamv12 <shivambca75@gmail.com>
2020-06-21 14:35:59 +0530

    Create Auth Controller Sign Up method

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+/** NPM Packages */
+/** Custom Packages */
+const authService = require('../services/authService');
+
+/**
+ * @param - name, email, password
+ * @desc - User Sign Up Functionality
+ */
+exports.signUp = async (req, res) => {
+  try {
+    const user = await authService.signUp(req.body);
+    if (!user)
+      return res.status(422).send({msg: 'User could not be registered.'});
+    return res
+      .status(200)
+      .send({msg: 'Successfully Registered.', data: req.body});
+  } catch (err) {
+    return res.status(500).send({msg: err.message});
+  }
+};
