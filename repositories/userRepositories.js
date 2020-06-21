99f81bb950d83e84119818d3abb5af3732dcf3e9
shivamv12 <shivambca75@gmail.com>
2020-06-21 17:24:16 +0530

    Add find one record query in user repository

--- repositories/userRepositories.js
+++ repositories/userRepositories.js
@@ -3,7 +3,13 @@
 const User = require('../models/User');
 
 /**
- * @param - payload (name, email, password, uid, slug)
- * @desc - Query to Insert user data
+ * @param - payload: <object> {name, email, password, uid, slug}
+ * @desc - Query to create new user record into database
  */
 exports.create = async (payload) => await User.create(payload);
+
+/**
+ * @param - filter: <object> eg. {email: <val>}
+ * @desc - Query to find user data, based on: email, uid, slug etc.
+ */
+exports.findOne = async (filter) => await User.findOne(filter);
