4bc940db95d9f37d558cdb399cb6b725f8f0923c
shivamv12 <shivambca75@gmail.com>
2020-06-22 17:06:03 +0530

    Implement passport local strategy for sign-in endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 /** NPM Packages */
+const passport = require('passport');
 const app = require('express').Router();
+
+require('../middlewares/passports/localStrategy');
 
 /** Custom Packages */
 const authRoute = require('./authRoute');
 
 app.all('/*', (req, res, next) => {
   if (req.path === '/') return res.status(200).send({msg: 'Server Root.'});
-  next();
+  else if (req.path === '/auth/sign-up') next();
+  else if (req.path === '/auth/sign-in')
+    passport.authenticate('local', {session: false}, (err, user, info) => {
+      if (!user || err || info != undefined)
+        return res.status(401).send({msg: 'Invalid Credentials.'});
+
+      req.user = user;
+
+      next();
+    })(req, res, next);
 });
 
 app.use('/auth', authRoute);
