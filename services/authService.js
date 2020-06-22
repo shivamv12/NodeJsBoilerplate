3cc7c60eedf04c5ba92d923ec4d69b66e9ca0723
shivamv12 <shivambca75@gmail.com>
2020-06-22 17:04:11 +0530

    Place get signed token method inside authService

--- services/authService.js
+++ services/authService.js
@@ -1,14 +1,24 @@
 /** NPM Packages */
+const JWT = require('jsonwebtoken');
+
 /** Custom Packages */
 const slugGenerator = require('../utils/slugGenerator');
 const randomGenerator = require('../utils/randomGenerator');
 const userRepo = require('../repositories/userRepositories');
 
 /**
+ * @desc - Generate signed JWT token. Payload: {id, email}
+ */
+exports.getSignedJwtToken = async (user) =>
+  await JWT.sign({uid: user.uid, email: user.email}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+
+/**
  * @desc - User Sign Up Service
  */
 exports.signUp = async (payload) => {
-  payload.uid = await randomGenerator({prefix: 'UID_', length: 8});
+  payload.uid = await randomGenerator({prefix: 'uid', length: 8});
   payload.slug = await slugGenerator(payload.name, {length: 5});
 
   let duplicate = await userRepo.findOne({email: payload.email});
