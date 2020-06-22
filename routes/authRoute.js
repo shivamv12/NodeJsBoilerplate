06bc32d2f5807b56b6616670cc6c51208e39e478
shivamv12 <shivambca75@gmail.com>
2020-06-22 17:05:24 +0530

    Create SignIn api endpoint

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,10 +2,14 @@
 const router = require('express').Router();
 
 /** Custom Packages */
-const {validateSignUp} = require('../middlewares/validators/authValidator');
-const {signUp} = require('../controllers/authController');
+const {
+  validateSignUp,
+  validateSignIn,
+} = require('../middlewares/validators/authValidator');
+const {signUp, signIn} = require('../controllers/authController');
 
 /** End Points */
 router.route('/sign-up').post(validateSignUp, signUp);
+router.route('/sign-in').post(validateSignIn, signIn);
 
 module.exports = router;
