cf69f4737aae31e20d0584e91dc1e0a69c854588
shivamv12 <shivambca75@gmail.com>
2020-06-22 20:42:32 +0530

    Implement heroku redis url in bullmq

--- configuration/bullmq.js
+++ configuration/bullmq.js
@@ -3,10 +3,12 @@ const {Queue} = require('bullmq');
 const IOredis = require('ioredis');
 
 /** Redis Connection */
-const conn = new IOredis(process.env.REDIS_PORT);
+const conn = new IOredis(process.env.REDIS_URL);
+
+console.log(conn.options);
 
 const bullmq = async () => {
-  const queue = new Queue('default', {connection: conn});
+  const queue = new Queue('default', {conn});
   return queue;
 };
 
